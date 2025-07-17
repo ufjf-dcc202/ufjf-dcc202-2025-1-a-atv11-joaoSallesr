2db6e30cee79dcf7fe2e46bad0e417615b8c1f5d
João Salles <joao.sallesr@gmail.com>
2025-07-17 16:28:48 -0300

    main atualizada

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 //main.js
 
+import { getLista, limpaLista } from "./lista.js";
+
 const olItens = document.querySelector("#itens");
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
+
+btnLimpar.addEventListener("click", limpaItensDaLista);
+
+function limpaItensDaLista() {
+    limpaLista();
+    atualizaLista();
+}
+
+atualizaLista();
+
+function atualizaLista() {
+    olItens.innerHTML = "";
+    let lista = getLista();
+    for (let i=0; i < lista.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = lista[i];
+        olItens.appendChild(li);
+    }
+}
